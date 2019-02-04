e7d31e0c7d13f078ae28b7bff072bf4cc0f982cf
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-02-04 14:55:28 +0300

    adding login, logout and signup functions

--- UI/assets/js/fetch.js
+++ UI/assets/js/fetch.js
@@ -1,3 +1,32 @@
+const userMessage = (message, colour) => {
+  let element = document.getElementById("dialog");
+  element.innerHTML =
+    `<div id="dialog-content">
+            <div id="dialog-text">` +
+    fomartErrorMessage(message) +
+    `</div>
+          </div>`;
+
+  document.getElementById("dialog-content").style.background = colour;
+  element.style.transform = "scale(1)";
+  element.style.msTransform = "scale(1)"; //ie9
+  element.style.WebkitTransform = "scale(1)"; //safari
+};
+
+const fomartErrorMessage = message => {
+  if (Array.isArray(message)) {
+    let newMessage = null;
+    for (let i = 0; i < message.length; i++) {
+      if (newMessage) newMessage += "<li>" + message[i] + "</li>";
+      else newMessage = "<li>" + message[i] + "</li>";
+    }
+
+    return "<ul>" + newMessage + "</ul>";
+  } else {
+    return message;
+  }
+};
+
 const getRedFlags = () => {
   // Add redflags from Api to web page
 
@@ -36,5 +65,105 @@ const getRedFlags = () => {
     })
     .catch(err => {
       console.log("Fetch Error :-S", err);
+      userMessage(err, "#f5313180");
+    });
+};
+
+const login = body => {
+  // post login
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: new Headers({
+      "content-Type": "application/json"
+    })
+  };
+
+  return fetch("http://127.0.0.1:5000/ireporter/api/v2/auth/login", options)
+    .then(response => response.json())
+    .then(data => {
+      if (data.status == 200) {
+        localStorage.setItem(
+          "currentUserIsAdmin",
+          data.data[0]["user"]["isAdmin"]
+        );
+        localStorage.setItem(
+          "currentUserUsername",
+          data.data[0]["user"]["username"]
+        );
+        localStorage.setItem("currentUserId", data.data[0]["user"]["id"]);
+        localStorage.setItem("currentUserToken", data.data[0]["token"]);
+
+        window.location.href = "./incidents/list.html";
+      } else {
+        userMessage(data.error, "#f5313180");
+      }
+    })
+    .catch(err => {
+      console.log("Fetch Error :-S", err);
+      userMessage(err, "#f5313180");
+    });
+};
+
+const logout = () => {
+  // logout
+
+  const options = {
+    method: "GET",
+    headers: new Headers({
+      "content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("currentUserToken")
+    })
+  };
+
+  return fetch("http://127.0.0.1:5000/ireporter/api/v2/auth/logout", options)
+    .then(response => response.json())
+    .then(data => {
+      if (data.message) {
+        localStorage.setItem("currentUserId", null);
+        localStorage.setItem("currentUserIsAdmin", null);
+        localStorage.setItem("currentUserUsername", null);
+        localStorage.setItem("currentUserToken", null);
+
+        window.location.href = "../index.html";
+      } else {
+        console.log("Logout Error see response object:-S ", data);
+        throw new Error("error logging out!");
+      }
+    })
+    .catch(err => {
+      console.log("Fetch Error :-S", err);
+      userMessage(err, "#f5313180");
+    });
+};
+
+const signup = body => {
+  // post login
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: new Headers({
+      "content-Type": "application/json"
+    })
+  };
+
+  return fetch("http://127.0.0.1:5000/ireporter/api/v2/auth/signup", options)
+    .then(response => response.json())
+    .then(data => {
+      if (data.status == 201) {
+        localStorage.setItem(
+          "userMassage",
+          "Account successfully created. Please login."
+        );
+        window.location.href = "index.html";
+      } else {
+        userMessage(data.error, "#f5313180");
+      }
+    })
+    .catch(err => {
+      console.log("Fetch Error: ", err);
+      userMessage(err, "#f5313180");
     });
 };
