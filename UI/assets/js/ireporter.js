25e4b801fafee32f337d2fce72055693e162ef3a
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-02-08 11:37:15 +0300

    Update ireporter.js

--- UI/assets/js/ireporter.js
+++ UI/assets/js/ireporter.js
@@ -15,3 +15,96 @@ window.onclick = event => {
     }
   }
 };
+
+const userMessage = (message, colour) => {
+  let element = document.getElementById("dialog");
+  element.innerHTML =
+    `<div id="dialog-content">
+            <div id="dialog-text">` +
+    fomartErrorMessage(message) +
+    `</div>
+          </div>`;
+
+  document.getElementById("dialog-content").style.background = colour;
+  element.style.transform = "scale(1)";
+  element.style.msTransform = "scale(1)"; //ie9
+  element.style.WebkitTransform = "scale(1)"; //safari
+};
+
+const fomartErrorMessage = message => {
+  if (Array.isArray(message)) {
+    let newMessage = null;
+    for (let i = 0; i < message.length; i++) {
+      if (newMessage) newMessage += "<li>" + message[i] + "</li>";
+      else newMessage = "<li>" + message[i] + "</li>";
+    }
+
+    return "<ul>" + newMessage + "</ul>";
+  } else {
+    return message;
+  }
+};
+
+const logout = () => {
+  // logout
+
+  const options = {
+    method: "GET",
+    headers: new Headers({
+      "content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("currentUserToken")
+    })
+  };
+
+  return fetch("http://127.0.0.1:5000/ireporter/api/v2/auth/logout", options)
+    .then(response => response.json())
+    .then(data => {
+      localStorage.setItem("currentUserId", null);
+      localStorage.setItem("currentUserIsAdmin", null);
+      localStorage.setItem("currentUserUsername", null);
+      localStorage.setItem("currentUserToken", null);
+
+      if (data.message) {
+        window.location.href = "../index.html";
+      } else {
+        console.log("Logout Error see response object:-S ", data);
+        throw new Error("error logging out!");
+      }
+    })
+    .catch(err => {
+      console.log("Fetch Error :-S", err);
+      window.location.href = "../index.html";
+    });
+};
+
+const logout2 = () => {
+  // logout
+
+  const options = {
+    method: "GET",
+    headers: new Headers({
+      "content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("currentUserToken")
+    })
+  };
+
+  return fetch("http://127.0.0.1:5000/ireporter/api/v2/auth/logout", options)
+    .then(response => response.json())
+    .then(data => {
+      localStorage.setItem("currentUserId", null);
+      localStorage.setItem("currentUserIsAdmin", null);
+      localStorage.setItem("currentUserUsername", null);
+      localStorage.setItem("currentUserToken", null);
+
+      if (data.message) {
+        window.location.href = "index.html";
+      } else {
+        console.log("Logout Error see response object:-S ", data);
+        throw new Error("error logging out!");
+      }
+    })
+    .catch(err => {
+      console.log("Fetch Error :-S", err);
+      window.location.href = "index.html";
+    });
+};
