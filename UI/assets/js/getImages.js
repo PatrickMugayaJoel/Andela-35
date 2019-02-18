c0a9013b72b1c548baf05a53276389315d885143
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-02-18 20:58:47 +0300

    Update getImages.js

--- UI/assets/js/getImages.js
+++ UI/assets/js/getImages.js
@@ -15,73 +15,44 @@ const getImages = () => {
         let images = data.data;
 
         let table = "";
-        if (images.length != 1) {
-          table = "<table><tr>";
-        } else {
-          table = `<table style="width:65%;"><tr>`;
-        }
 
         for (let i = 0; i < images.length; i++) {
-          if (i % 3 == 2) {
-            table +=
-              `<td>
-              <a href="https://challenge-four.herokuapp.com/ireporter/api/v2/files/images/` +
-              images[i] +
-              `">
-            <img
-                src="https://challenge-four.herokuapp.com/ireporter/api/v2/files/images/` +
-              images[i] +
-              `"
-            /><a>
-            </td></tr><tr>`;
-          } else {
-            table +=
-              `<td>
-              <a href="https://challenge-four.herokuapp.com/ireporter/api/v2/files/images/` +
-              images[i] +
-              `">
-            <img
-                src="https://challenge-four.herokuapp.com/ireporter/api/v2/files/images/` +
-              images[i] +
-              `"
-            /><a>
-            </td>`;
-          }
+          table +=
+            `
+          <a
+          href="https://challenge-four.herokuapp.com/ireporter/api/v2/files/images/` +
+            images[i] +
+            `"
+        >
+          <div
+            class="showimgs"
+            style="background-image: url('https://challenge-four.herokuapp.com/ireporter/api/v2/files/images/` +
+            images[i] +
+            `'), url('../../assets/images/image-not-found.jpg')"
+          ></div>
+        </a>
+        `;
         }
 
         table += `
-              <td id="uploading">
-                <div onclick="document.getElementById('file').click()">
-                  <img src="../../assets/images/add.svg" alt="image" />
-                </div>
-              </td>
-            </tr>
-          </table>`;
+        <div id="uploading" onclick="document.getElementById('file').click()">
+        <img src="../../assets/images/add.svg" alt="image" />
+      </div>`;
 
         document.getElementById("showimgs").innerHTML = table;
       } else if (data.error) {
         userMessage(data.error, "rgb(224, 35, 35)");
-        document.getElementById("showimgs").innerHTML = `<table>
-          <tr>
-          <td id="uploading">
-            <div onclick="document.getElementById('file').click()">
-              <img src="../../assets/images/add.svg" alt="image" />
-            </div>
-          </td>
-        </tr>
-      </table>
+        document.getElementById("showimgs").innerHTML = `
+        <div id="uploading" onclick="document.getElementById('file').click()">
+        <img src="../../assets/images/add.svg" alt="image" />
+      </div>
         `;
       } else {
         console.log(data);
-        document.getElementById("showimgs").innerHTML = `<table>
-          <tr>
-          <td id="uploading">
-            <div onclick="document.getElementById('file').click()">
-              <img src="../../assets/images/add.svg" alt="image" />
-            </div>
-          </td>
-        </tr>
-      </table>
+        document.getElementById("showimgs").innerHTML = `
+        <div id="uploading" onclick="document.getElementById('file').click()">
+        <img src="../../assets/images/add.svg" alt="image" />
+      </div>
         `;
       }
     })
